a0fdf9d8040327be57f1d25bf7e17dcec51f3e34
sriramiotlabs <sriramsanthosh04@gmail.com>
2026-04-18 14:33:55 +0530

    Updated alignments and added comments for the code

--- ard_glass.ino
+++ ard_glass.ino
@@ -1,14 +1,15 @@
+/* include this for i2c caling i2c functions */
 #include <Wire.h>
+/* include this for recieving incoming data from smartphone */
 #include "BluetoothSerial.h"
-BluetoothSerial btSerial;
+/* initialize bluetooth class handle */
+BluetoothSerial btSerial; 
+/* include for using bigger version of 128x64 oled display */
 #include <GyverOLED.h>
-//#include <U8g2lib.h>
-#include <Wire.h>
-
+/* include these libraries for graphics related functions */
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
-//#include <Adafruit_SSD1306.h>
-#include <Wire.h>
+/*select the oled display resolution based on your oled model*/
 //GyverOLED<SSD1306_128x32, OLED_BUFFER> oled;
 //GyverOLED<SSD1306_128x32, OLED_NO_BUFFER> oled;
 //GyverOLED<SSD1306_128x64, OLED_BUFFER> oled;
@@ -18,12 +19,15 @@ GyverOLED<SSH1106_128x64> display;
 // можно передать адрес: GyverOLED oled(0x3C);
 // Pins
 
-//#include <Adafruit_GFX.h>
 
+/* set screen width*/
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-#define OLED_RESET -1
-//Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+String callline;
+String currentData = "";
+String inputString="";
+/* bitmap images of various functions in hex format as display expects in hex format */
+/* Refer readme for more info in generating hex based images */
 const uint8_t set [] PROGMEM = {
 0x00, 0x00, 0x00, 0x00, 0x70, 0xD8, 0x8C, 0x0C, 0x18, 0x10, 0x1E, 0x0F, 0x01, 0x01, 0x03, 0x1E,
 0x10, 0x18, 0x0C, 0x8C, 0xD8, 0x70, 0x00, 0x00, 0x00, 0x00, 0x3C, 0x66, 0x42, 0xC3, 0x81, 0x00,
@@ -70,133 +74,60 @@ const uint8_t call [] PROGMEM = {
 String notification;
 void setup() {
   // put your setup code here, to run once:
-// oled.init();
-// display.setWrap(true); // Enable word wrap
+/* initialize the display */
   display.init();
-//setTime(12,19,0,5,8,22); 
-  Serial.begin(9600);
+  Serial.begin(9600); /* Set baud rate */
   // Set interrupt pin as input
- Wire.begin();
- pinMode(2,OUTPUT);
-//  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-display.clear();
-// display.drawBitmap(50, 33, right,24, 24);
-  
- // display.clear();
- // display.drawBitmap(60, 60, right, 64, 64);
+  /* initialize i2c functions for display */
+  Wire.begin();
+  pinMode(2,OUTPUT);
+/* Clear initial contents */
+  display.clear();
+  /* Initialize bluetooth name to be indentified in your android app you can change the discovery name */
   btSerial.begin("navihud");
- 
+  /* set the cursor to be printed */
   display.setCursor(30,0);
   display.print("connect to app");
   display.setCursor(0,2);
   display.print("to start your journey");
   display.update();
 }
-String callline;
-String currentData = "";
-String inputString="";
+
 void loop() {
   // put your main code here, to run repeatedly:
-   
-   String receivedData;
-   const int maxCharsPerLine = 21;
-//display.clear();
-
-//  display.drawBitmap(50, 24, right, 128, 64);
-  //oled.drawBitmap(50, 24, bitmap_32x32, 32, 32)
- /* if(Serial.available()){
-  receivedData=Serial.readStringUntil('\n');
-
-  }*/
-if (btSerial.available()>0) {
-    //char incomingChar = btSerial.read();
-   // String recchar=btSerial.readString();
- //  inputString="";
-   
-    // receivedData = btSerial.readStringUntil('\n');
-   // Serial.println(receivedData);
-   // inputString=receivedData;
-  
-   /*// Serial.println(receivedData);
-    delay(1000);
-    //int textLength = inputString.length();
- int textLength = inputString.length();
-  int currentLine = 0;
-
-   for (int i = 0; i < textLength; i += maxCharsPerLine) {
-    String line = inputString.substring(i, i + maxCharsPerLine);
-    display.print(line);
-    display.update();
-    delay(1000); // Adjust the delay time for scrolling speed
-
-    if (i + maxCharsPerLine < textLength) {
-      display.clear();
-      delay(1000); // Delay to show a new line
-    }
-  }*/
-  
-  //display.print(inputString);
-//  display.update();
-
-display.clear();
-inputString = btSerial.readStringUntil('\n');
-//display.setCursor(30,4);
-    //display.print(inputString);
-    
-   if(inputString.startsWith("HUD")){
-   // display.clear();
-    //display.setCursor(30,4);
-   // display.print("connected!");
-    int hIndex = inputString.indexOf("HUD");
-
-    if (hIndex != -1) {
-      // "null" is found, so print the part before "null"
-      String hNull = inputString.substring(0, hIndex);
-    //display.update();
+  String receivedData;
+  const int maxCharsPerLine = 21;
+  if (btSerial.available()>0) {
+    int currentLine = 0;
     display.clear();
-    
-    display.setCursor(0,0);
-    display.print("");
-    display.setCursor(30,4);
-    display.print("connected!");
-    
-   }
-   }
-    
-   // 
-    //delay(6000);
-    // Display the received data with word wrapping
-    //int textLength = inputString.length();
-    int nullIndex = inputString.indexOf("null");
+    inputString = btSerial.readStringUntil('\n');
+    if(inputString.startsWith("HUD")){
+      int hIndex = inputString.indexOf("HUD");
 
+      if (hIndex != -1) {
+        // "null" is found, so print the part before "null"
+        String hNull = inputString.substring(0, hIndex); 
+        display.clear();
+        display.setCursor(0,0);
+        display.print("");
+        display.setCursor(30,4);
+        display.print("connected!");
+      }
+    }
+    // Display the received data with word wrapping
+    int nullIndex = inputString.indexOf("null");
     if (nullIndex != -1) {
       // "null" is found, so print the part before "null"
       String partBeforeNull = inputString.substring(0, nullIndex);
-      
       Serial.println("Received: " + partBeforeNull);
       int textLength = partBeforeNull.length();
       display.setCursor(0,0);
-    for (int i = 0; i < textLength; i += maxCharsPerLine) {
-      String line = partBeforeNull.substring(i, min(i + maxCharsPerLine, textLength));
-      display.println(line);
-    }
-    } else {
-      // "null" is not found, so print the entire received string
-     // Serial.println("Received: " + inputString);
-    /* int textLength = inputString.length();
-      display.setCursor(0,0);
-    for (int i = 0; i < textLength; i += maxCharsPerLine) {
-      String line = inputString.substring(i, min(i + maxCharsPerLine, textLength));
-      display.println(line);
-    }*/
-    }
-  
-   /* display.setCursor(0,0);
-    for (int i = 0; i < textLength; i += maxCharsPerLine) {
-      String line = inputString.substring(i, min(i + maxCharsPerLine, textLength));
-      display.println(line);
-    }*/
-     if (inputString.indexOf("right onto") != -1) {
+      for (int i = 0; i < textLength; i += maxCharsPerLine) {
+        String line = partBeforeNull.substring(i, min(i + maxCharsPerLine, textLength));
+        display.println(line);
+      }
+    } 
+    if (inputString.indexOf("right onto") != -1) {
       // Perform an action for "turn right"
       // For example, you can control a motor or an LED.
       Serial.println("Turning right");
@@ -216,84 +147,66 @@ inputString = btSerial.readStringUntil('\n');
       Serial.println("Turning straight");
       display.drawBitmap(50, 33, straight,24, 24);
     }
-   
     if (inputString.indexOf("u-turn") != -1 || inputString.indexOf("U-turn") != -1) {
       // Perform an action for "turn left"
       // For example, you can control a motor or an LED.
       Serial.println("Turning straight");
       display.drawBitmap(50, 33, uturn,24, 24);
     }
-       if (inputString.indexOf("destination") != -1) {
+    if (inputString.indexOf("destination") != -1) {
       // Perform an action for "turn left"
       // For example, you can control a motor or an LED.
       Serial.println("destination arrived");
       digitalWrite(2,HIGH);
-
       delay(1000);
-
       digitalWrite(2,LOW);
       delay(1000);
       digitalWrite(2,HIGH);
-
       delay(1000);
-
       digitalWrite(2,LOW);
       delay(1000);
       digitalWrite(2,HIGH);
-
       delay(1000);
-
       digitalWrite(2,LOW);
       delay(1000);
       digitalWrite(2,HIGH);
-
       delay(1000);
-
-      digitalWrite(2,LOW);
-       delay(1000);
-      digitalWrite(2,HIGH);
-
-      delay(1000);
-
       digitalWrite(2,LOW);
       delay(1000);
       digitalWrite(2,HIGH);
-
       delay(1000);
-
       digitalWrite(2,LOW);
-     
+      delay(1000);
+      digitalWrite(2,HIGH);
+      delay(1000);
+      digitalWrite(2,LOW);
     }
     int callIndex = inputString.indexOf("call");
-
     if (callIndex != -1) {
       // "null" is found, so print the part before "null"
       String callNull = inputString.substring(0, callIndex);
       
       Serial.println("Received: " +callNull);
       int callLength = callNull.length();
-   //   display.setCursor(0,0);
-    for (int i = 0; i < callLength; i += maxCharsPerLine) {
-      String callline = callNull.substring(i, min(i + maxCharsPerLine, callLength));
-     // display.println(line);
+      for (int i = 0; i < callLength; i += maxCharsPerLine) {
+        String callline = callNull.substring(i, min(i + maxCharsPerLine, callLength));
+      }
+      if (callline.indexOf("incoming")) {
+        // Perform an action for "turn left"
+        // For example, you can control a motor or an LED.
+        display.clear();
+        display.drawBitmap(50, 9, call,24, 24);
+        display.setCursor(30,6);
+        display.print("incoming call");
+      }
     }
-    
-     
-    if (callline.indexOf("incoming")) {
-      // Perform an action for "turn left"
-      // For example, you can control a motor or an LED.
-     display.clear();
-      display.drawBitmap(50, 9, call,24, 24);
-       display.setCursor(30,6);
-      display.print("incoming call");
-    }
-    }
+    /* built in function that updates the display used along with print udated commony here */
     display.update();
-} 
+  } 
    currentData = receivedData;
    
 
 
 
-//display.clear();
+
 }
